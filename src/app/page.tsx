5279ccd9977a6bb6ed2ac888a97320baf2281dde
Calcifer1001 <dseidlertoken@gmail.com>
2023-11-07 10:32:22 -0300

    [MOD] version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { keyStores } from 'near-api-js';
 import { useWalletSelector } from './WalletSelectorContext'
 
 export default function Home() {
@@ -6,22 +7,55 @@ export default function Home() {
   const { selector, modal } = useWalletSelector()
   const handleSignIn = () => {
     modal.show();
-  };  
+  };
 
   const handleSignOut = async () => {
     const w = await selector.wallet()
     w.signOut()
   }
-  if (selector && selector.isSignedIn()) {
-    return (<>
-      <button>Stake 1 NEAR</button>
-      <button onClick={() => handleSignOut()}>Disconnect</button>
-    </>)
-  } else {
-    return <button
-      onClick={() => handleSignIn()}
 
+  const stake = async () => {
+    const w = await selector.wallet()
+    const r = await w.signAndSendTransaction({
+      actions: [
+        {
+          type: "FunctionCall",
+          params: {
+            methodName: "deposit_and_stake",
+            args: {},
+            gas: "30000000000000",
+            deposit: "1" + "0".repeat(24),
+          },
+        },
+      ],
+    });
+    console.log(1, r)
+
+  }
+  if (selector && selector.isSignedIn()) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button onClick={stake} style={{ width: "500px", fontSize: "2em", margin: "1em" }}>Stake 1 NEAR</button>
+        <button onClick={() => handleSignOut()} style={{ width: "500px", fontSize: "2em" }}>Disconnect</button>
+      </div>)
+  } else {
+    return <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ><button
+      onClick={() => handleSignIn()}
+      style={{ width: "500px", fontSize: "2em", margin: "1em" }}
     >Connect</button>
+    </div>
   }
 
 }
